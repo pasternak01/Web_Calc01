89aa48480528ede0e9e64cace387934ed4f7f507
kpast <k.pasternak01@gmail.com>
2025-04-18 12:49:40 +0300

    v 0.1

---
+++ Readme.md
@@ -0,0 +1,4 @@
+## Calculator
+***
+
+I was created when I studied HTML, CSS, Flask. Their intensity, requests, etc.
---
+++ main.py
@@ -0,0 +1,49 @@
+from flask import Flask, render_template, request, session
+from livereload import Server
+from dotenv import load_dotenv
+import os
+load_dotenv()
+
+app = Flask(__name__)
+app.secret_key = os.getenv("SECRET_KEY")
+
+
+@app.route("/", methods=["GET", "POST"])
+def home():
+    try:
+        print("⭐", session["result"])
+    except Exception:
+        print("❌No session result")
+    if "result" not in session:
+        session['result'] = ""
+
+    if request.method == "POST":
+        value = request.form.get("button")
+        print(type(value))
+        # session["value_type"] = type(value)
+
+        if value == "C":
+            session["result"] = ""
+        elif value == "=":
+            try:
+                session["result"] = f"{eval(session["result"])}"
+                print(f"eval done {session['result']}")
+            except Exception:
+                print("❌ Houston we have problem ❌")
+                session["result"] = "ERROR"
+        elif value == "<":
+            session['result'] = session['result'][:-1]
+
+
+        else:
+            print(f"{session['result']} appending {value}")
+            session["result"] += str(value)
+
+    return render_template("index.html", result=session["result"])
+
+
+if __name__ == "__main__":
+    server = Server(app.wsgi_app)
+    server.watch("templates/")
+    server.watch("static/")
+    server.serve(debug=True, port=5000)
